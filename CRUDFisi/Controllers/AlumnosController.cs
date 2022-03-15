0d5279ce37c28c0ed137c6998820d41286b61d78
Piero Criales <55029565+GabrielC29@users.noreply.github.com>
2022-03-14 19:32:47 -0500

    Agregar archivos de proyecto.

---
+++ CRUDFisi/Controllers/AlumnosController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUDFisi.Models;
+using CRUDFisi.Models.ViewModels;
+
+namespace CRUDFisi.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        // GET: Alumnos
+        public ActionResult Index()
+        {
+
+            List<ListAlumnosViewModel> lst;
+            using(FisiEntities db = new FisiEntities())
+            {
+                lst = (from p in db.alumnos
+                          select new ListAlumnosViewModel
+                          {
+                              Id = p.id,
+                              Nombre = p.nombre,
+                              Apellido = p.apellido,
+                              Codigo = p.codigo,
+
+                          }).ToList();
+            }
+
+            return View(lst);
+        }
+        
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(AlumnosViewModel model)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    using(FisiEntities db = new FisiEntities())
+                    {
+                        var oAlumnos = new alumnos();
+                        oAlumnos.nombre = model.Nombre;
+                        oAlumnos.apellido = model.Apellido;
+                        oAlumnos.codigo = model.Codigo;
+
+                        db.alumnos.Add(oAlumnos);
+                        db.SaveChanges();
+                    }
+
+                    return Redirect("~/Alumnos/");
+
+                }
+
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public ActionResult Editar(int id)
+        {
+            AlumnosViewModel model = new AlumnosViewModel();
+            using(FisiEntities db = new FisiEntities())
+            {
+                var oAlumnos = db.alumnos.Find(id);
+                model.Nombre = oAlumnos.nombre;
+                model.Apellido = oAlumnos.apellido;
+                model.Codigo = oAlumnos.codigo;
+                model.Id = oAlumnos.id;
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(AlumnosViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (FisiEntities db = new FisiEntities())
+                    {
+                        var oAlumnos = db.alumnos.Find(model.Id);
+                        oAlumnos.nombre = model.Nombre;
+                        oAlumnos.apellido = model.Apellido;
+                        oAlumnos.codigo = model.Codigo;
+
+                        db.Entry(oAlumnos).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                    }
+
+                    return Redirect("~/Alumnos/");
+
+                }
+
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Eliminar(int id)
+        {
+            AlumnosViewModel model = new AlumnosViewModel();
+            using (FisiEntities db = new FisiEntities())
+            {
+                var oAlumnos = db.alumnos.Find(id);
+                db.alumnos.Remove(oAlumnos);
+                db.SaveChanges();
+            }
+
+            return Redirect("~/Alumnos/");
+        }
+    }
+}
---
+++ CRUDFisi/Models/ViewModels/AlumnosViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUDFisi.Models.ViewModels
+{
+    public class AlumnosViewModel
+    {
+        
+        public int Id { get; set; }
+        [Required]
+        [StringLength(20)]
+        [Display(Name ="Nombre")]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(20)]
+        [Display(Name ="Apellido")]
+        public string Apellido { get; set; }
+        [Required]
+        [StringLength(8)]
+        [Display(Name ="Código")]
+        public string Codigo { get; set; }
+
+    }
+}
